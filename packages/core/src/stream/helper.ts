2d5bbfafbc9326386c8a9389ff349c54813ab3c1
jdy8739 <jdy8739@naver.com>
2026-05-23 16:08:27 +0900

    fix(core): remove terminal flush in progress stream

--- packages/core/src/stream/helper.ts
+++ packages/core/src/stream/helper.ts
@@ -3,16 +3,20 @@
  * 브라우저가 Fetch를 통한 ReadableStream 업로드를 지원하는지 확인합니다.
  */
 export const checkSupportsStreamingUpload = (url: string) => {
-  let duplexAccessed = false;
+  try {
+    let duplexAccessed = false;
 
-  const hasContentType = new Request(url, {
-    body: new ReadableStream(),
-    method: "POST",
-    get duplex(): "half" {
-      duplexAccessed = true;
-      return "half";
-    },
-  }).headers.has("Content-Type");
+    const hasContentType = new Request(url, {
+      body: new ReadableStream(),
+      method: "POST",
+      get duplex(): "half" {
+        duplexAccessed = true;
+        return "half";
+      },
+    }).headers.has("Content-Type");
 
-  return duplexAccessed && !hasContentType;
+    return duplexAccessed && !hasContentType;
+  } catch {
+    return false;
+  }
 };
